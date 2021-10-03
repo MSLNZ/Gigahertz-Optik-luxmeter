c737d195498faded65d6f54a1a370ae5fc421e8b
yangyenn <yangyenn_tyy@hotmail.com>
2021-10-04 10:35:04 +1300

    Using Qtwidgets, added black screen

--- Gigahertz-Optik main.py
+++ Gigahertz-Optik main.py
@@ -1,9 +1,6 @@
-import time
 from datetime import datetime
-import keyboard
-import os
-from msl.qt import prompt
 
+from msl.qt import QtWidgets, QtCore, prompt
 from Controller import Optik64
 
 serial = '14195'
@@ -32,7 +29,7 @@ optik.set_azmode(az_mode)
 # if optik.get_quantity(calnum) != 'E':
 #     raise ValueError('not measuring illuminance')
 
-file ='GlycoSyn_Precision Air_cal.csv'
+file ='TechRentals cal.csv'
 
 ref_plane_count = 0
 lux_count = 0
@@ -42,61 +39,128 @@ print('ready')
 
 with open(file,mode='a') as fp:
     fp.write('\n' + datetime.now().replace(microsecond=0).isoformat(sep=' ')+'\n')
-while True:
-    if keyboard.is_pressed('t'):
+
+
+def r_pressed():
+    global ref_plane_count
+    with open(file, mode='a') as fp:
+        text = prompt.text('Enter distance (m)', title=None, font=24, value='', multi_line=False, echo=0)
+        if not text:
+            return
         optik.set_spectral(False)
+        ref_plane_count += 1
         z = []
+        lux = [f'{text} m run {ref_plane_count:02}']
+        # fp.write('\n')
         for i in range(lux_measurements):
             optik.measure()
             print(optik.get_cwvalue())
+            lux.append(str(optik.get_cwvalue()))
             z.append(optik.get_cwvalue())
         a = sum(z) / 10
         print('Mean: {}'.format(a))
+        fp.write(','.join(lux) + '\n')
         print('done')
-    if keyboard.is_pressed('r'):
-        with open(file, mode='a') as fp:
-            text = prompt.text('Enter distance (m)', title=None, font=24, value='', multi_line=False, echo=0)
-            if not text: continue
-            optik.set_spectral(False)
-            ref_plane_count += 1
-            lux = [f'{text} m run {ref_plane_count:02}']
-            # fp.write('\n')
-            for i in range(lux_measurements):
-                optik.measure()
-                print(optik.get_cwvalue())
-                lux.append(str(optik.get_cwvalue()))
-            fp.write(','.join(lux)+'\n')
-            print('done')
-    if keyboard.is_pressed('l'):
-        with open(file, mode='a') as fp:
-            text = prompt.text('Enter lux value (lx)', title=None, font=24, value='', multi_line=False, echo=0)
-            if not text: continue
-            optik.set_spectral(False)
-            #optik.set_spectral(True)
-            lux_count += 1
-            lux = [f'{text} lux run {lux_count:02}']
-            # fp.write('\n')
-            for i in range(lux_measurements):
-                optik.measure()
-                print(optik.get_cwvalue())
-                lux.append(str(optik.get_cwvalue()))
-            fp.write(','.join(lux)+'\n')
-            print('done')
-    if keyboard.is_pressed(('c')):
-        with open(file, mode='a') as fp:
-            text = prompt.text('Enter lux value/distance ', title=None, font=24, value='', multi_line=False, echo=0)
-            if not text: continue
-            optik.set_spectral(True)
-            CCT_count += 1
-            colour = [f'CCT run {CCT_count:02} at {text}']
-            # fp.write('\n')
-            for i in range(CCT_measurements):
-                optik.measure()
-                print(optik.get_colour()[7])
-                colour.append(str(optik.get_colour()[7]))
-            fp.write(','.join(colour) + '\n')
-            print('done')
-    elif keyboard.is_pressed('s'):
-        break
+
+
+def t_pressed():
+    optik.set_spectral(False)
+    z = []
+    for i in range(lux_measurements):
+        optik.measure()
+        print(optik.get_cwvalue())
+        z.append(optik.get_cwvalue())
+    a = sum(z) / 10
+    print('Mean: {}'.format(a))
+    print('done')
+
+def l_pressed():
+    global lux_count
+    with open(file, mode='a') as fp:
+        text = prompt.text('Enter lux value (lx)', title=None, font=24, value='', multi_line=False, echo=0)
+        if not text:
+            return
+        optik.set_spectral(False)
+        #optik.set_spectral(True)
+        lux_count += 1
+        z = []
+        lux = [f'{text} lux run {lux_count:02}']
+        # fp.write('\n')
+        for i in range(lux_measurements):
+            optik.measure()
+            print(optik.get_cwvalue())
+            lux.append(str(optik.get_cwvalue()))
+            z.append(optik.get_cwvalue())
+        a = sum(z) / 10
+        print('Mean: {}'.format(a))
+        fp.write(','.join(lux)+'\n')
+        print('done')
+
+def c_pressed():
+    global CCT_count
+    with open(file, mode='a') as fp:
+        text = prompt.text('Enter lux value/distance ', title=None, font=24, value='', multi_line=False, echo=0)
+        if not text:
+            return
+        optik.set_spectral(True)
+        CCT_count += 1
+        z = []
+        colour = [f'CCT run {CCT_count:02} at {text}']
+        # fp.write('\n')
+        for i in range(CCT_measurements):
+            optik.measure()
+            print(optik.get_colour()[7])
+            colour.append(str(optik.get_colour()[7]))
+            z.append(optik.get_colour()[7])
+        a = sum(z) / 5
+        print('Mean: {}'.format(a))
+        fp.write(','.join(colour) + '\n')
+        print('done')
+
+
+class BlackScreen(QtWidgets.QWidget):
+
+    def __init__(self):
+        """Make the desktop screen black."""
+        super(BlackScreen, self).__init__()
+        self.setWindowTitle('Press Esc to toggle')
+        self.setStyleSheet('background-color:black;color:white')
+        self.showMaximized()
+
+    def black(self):
+        self.showFullScreen()
+
+    def toggle(self):
+        """Toggle between full screen and normal display."""
+        if self.isFullScreen():
+            self.showNormal()
+        else:
+            self.showFullScreen()
+
+    def keyPressEvent(self, event):
+        """Overrides :meth:`QtWidgets.QtWidget.keyPressEvent`."""
+        super(BlackScreen, self).keyPressEvent(event)
+        if event.key() == QtCore.Qt.Key_Escape:
+            self.toggle()
+        elif event.key() == QtCore.Qt.Key_T:
+            t_pressed()
+        elif event.key() == QtCore.Qt.Key_R:
+            self.toggle()
+            r_pressed()
+            self.toggle()
+        elif event.key() == QtCore.Qt.Key_C:
+            self.toggle()
+            c_pressed()
+            self.toggle()
+        elif event.key() == QtCore.Qt.Key_L:
+            self.toggle()
+            l_pressed()
+            self.toggle()
+
+
+app = QtWidgets.QApplication()
+bs = BlackScreen()
+bs.show()
+app.exec()
 
 optik.release_handle()
